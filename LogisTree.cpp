a3306516eac946d096cca4113500745d8b2d3da1
德国中文书店 <61420993+zhongwenshu@users.noreply.github.com>
2020-02-24 16:13:57 +0100

    header

--- LogisTree.cpp
+++ LogisTree.cpp
@@ -7,6 +7,11 @@
 
 //物流表达式
 //%铁路(中通6666 + 圆通YT(8888 + 5641)) + %空运(JD0001-1-1)
+//这是免费软件，可以更改源码，但请保留此头部说明，谢谢
+
+//logistics expression
+//%railway(zhongtong6666 + yuantong(8888 + 5641)) + %airline(JD0001-1-1)
+//it is freeware, you could edit the source code, but please remain this header, thank you
 
 #include "stdafx.h"
 #include <algorithm>
